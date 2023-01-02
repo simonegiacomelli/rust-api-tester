21cb7727e2efada6e96c77916f77ae8e812967a8
Simone <simone.giacomelli@gmail.com>
2023-01-02 21:27:00 +0000

    first http get ok

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,17 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+async fn get(url: &str) -> String {
+    reqwest::get(url).await.unwrap().text().await.unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn it_works() {
+        let actual = get("http://httpbin.org/json").await;
+        assert!(actual.contains("Sample Slide Show"))
     }
 }

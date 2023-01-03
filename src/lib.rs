14ab62bd5193e80aeba24b346ca45b149c175018
Simone <simone.giacomelli@gmail.com>
2023-01-03 19:56:24 +0000

    used blocking api

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,19 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-async fn get(url: &str) -> String {
-    reqwest::get(url).await.unwrap().text().await.unwrap()
+
+fn get(url: &str) -> String {
+    reqwest::blocking::get(url).unwrap().text().unwrap()
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[tokio::test]
-    async fn it_works() {
-        let actual = get("http://httpbin.org/json").await;
+    #[test]
+    fn it_works() {
+        let actual = get("http://httpbin.org/json");
         assert!(actual.contains("Sample Slide Show"))
     }
 }
